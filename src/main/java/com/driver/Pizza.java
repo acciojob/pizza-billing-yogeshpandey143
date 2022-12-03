bd0d3a601c4e6f8fa704e88aee3085c9c93f492b
Yogesh <bc21143@smsvaranasi.in>
2022-12-03 17:13:51 +0530

    pizza bill initial

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -1,34 +1,87 @@
 package com.driver;
 
 public class Pizza {
+	  private int price;
+	    private int nprice;
+	    private Boolean isVeg;
+	    private String bill;
+	    private boolean takeAway = false; 
+	    private boolean addCheese = false; 
+	    private boolean addTop = false; 
 
-    private int price;
-    private Boolean isVeg;
-    private String bill;
+	    public Pizza(Boolean isVeg){
+	        this.isVeg = isVeg;
+	        // your code goes here
+	    }
 
-    public Pizza(Boolean isVeg){
-        this.isVeg = isVeg;
-        // your code goes here
-    }
+	    public int getPrice(){
+	    	
+	    	if(isVeg)
+	    	{
+	    		this.price= this.price+300;
+	    	
+	    	}
+	    	else
+	    	{
+	    		this.price= this.price+400;
+	    		
+	    		
+	    	}
+	    	
+	        return this.price;
+	    }
 
-    public int getPrice(){
-        return this.price;
-    }
+	    public void addExtraCheese(){
+	    	if(this.addCheese==false)
+	    	{
+	        this.addCheese = true;
+	    	
+	    	this.nprice= this.nprice+80;
+	    	}
+	        
+	    }
 
-    public void addExtraCheese(){
-        // your code goes here
-    }
+	    public void addExtraToppings(){
+	    	
+	    	if(isVeg)
+	    	{
+	    	if(this.addTop==false)
+	    	{
+	       this.addTop= true;
+	    	
+	    	this.nprice= this.nprice+70;
+	    	}
+	    	}
+	    	else
+	    	{
+	    		if(this.addTop==false)
+	    		{
+	    		       this.addTop= true;
+	    		    	
+	    		    	this.nprice= this.nprice+120;	
+	    		}
+	    	}
+	    }
 
-    public void addExtraToppings(){
-        // your code goes here
-    }
+	    public void addTakeaway(){
+	    	if(this.takeAway==false)
+	    	{
+	       this.takeAway = true;
+	    	this.nprice= this.nprice+20;
+	    	}
+	    }
 
-    public void addTakeaway(){
-        // your code goes here
-    }
-
-    public String getBill(){
-        // your code goes here
-        return this.bill;
-    }
+	    public String getBill(){
+	    	if(isVeg)
+	    	{
+	    		this.nprice = this.nprice+300;
+	     this.bill = Integer.toString(this.nprice);
+	    	}
+	    	else
+	    	{
+	    		this.nprice = this.nprice+400;
+	    	     this.bill = Integer.toString(this.nprice);	
+	    	}
+	        return this.bill;
+}
 }
